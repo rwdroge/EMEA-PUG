6b160c5114cdcd98603366cd36e110899c634604
Ruben Dröge <rdroge@progress.com>
2015-11-02 16:13:31 +0000

    Changes made by Ruben Dröge (rdroge@progress.com).

--- PUG EMEA Hybrid/components/order2quotaView/index.js
+++ PUG EMEA Hybrid/components/order2quotaView/index.js
@@ -1,12 +1,10 @@
 'use strict';
 
-app.salesrepListView = kendo.observable({
+app.order2quoteView= kendo.observable({
     onShow: function() {},
     afterShow: function() {}
 });
 
-// START_CUSTOM_CODE_salesrepListView
-// END_CUSTOM_CODE_salesrepListView
 (function(parent) {
     var dataProvider = app.data.sports2000New,
         jsdoOptions = {
@@ -16,15 +14,6 @@ app.salesrepListView = kendo.observable({
         dataSourceOptions = {
             type: 'jsdo',
             transport: {},
-
-            change: function(e) {
-                var data = this.data();
-                for (var i = 0; i < data.length; i++) {
-                    var dataItem = data[i];
-
-                    flattenLocationProperties(dataItem);
-                }
-            },
             schema: {
                 model: {
                     fields: {
@@ -47,22 +36,18 @@ app.salesrepListView = kendo.observable({
         dataSource = new kendo.data.DataSource({
             pageSize: 50
         }),
-        salesrepListViewModel = kendo.observable({
+        order2quoteViewModel = kendo.observable({
             dataSource: dataSource,
             dataSourceOptions: dataSourceOptions,
             jsdoOptions: jsdoOptions,
             itemClick: function(e) {
                 app.mobileApp.navigate('#components/salesrepListView/details.html?uid=' + e.dataItem.uid);
             },
-            photoAs64: function(e) {
-                return "data:image/jpeg;base64," + this.get(e);
-            },
             detailsShow: function(e) {
                 var item = e.view.params.uid,
                     dataSource = salesrepListViewModel.get('dataSource'),
                     itemModel = dataSource.getByUid(item);
-                console.log(itemModel.Image);
-                	itemModel.ImageUrl = "data:image/png;base64," + itemModel.Image;
+                itemModel.ImageUrl = processImage(itemModel.Image);
                 if (!itemModel.RepName) {
                     itemModel.RepName = String.fromCharCode(160);
                 }
@@ -85,18 +70,5 @@ app.salesrepListView = kendo.observable({
         });
     });
 
-})(app.salesrepListView);
+})(app.order2quoteView);
 
-// START_CUSTOM_CODE_salesrepListViewModel
-// you can handle the beforeFill / afterFill events here. For example:
-/*
-app.salesrepListView.salesrepListViewModel.jsdoOptions.events = {
-    'beforeFill' : [ {
-        scope : app.salesrepListView.salesrepListViewModel,
-        fn : function (jsdo, success, request) {
-            // beforeFill event handler statements ...
-        }
-    } ]
-};
-*/
-// END_CUSTOM_CODE_salesrepListViewModel
